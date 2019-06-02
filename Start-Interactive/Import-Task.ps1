54121be16bfbc47a40375ef53c20078f4f0d7047
MattiasC85 <44426149+MattiasC85@users.noreply.github.com>
2019-06-03 00:29:38 +0200

    Add files via upload

---
+++ Start-Interactive/Import-Task.ps1
@@ -0,0 +1,35 @@
+ Param(
+
+   [Parameter(Mandatory=$true)]
+   $Name,
+   [Parameter(Mandatory=$true)]
+   $FullPathToExe,
+   [Parameter(Mandatory=$false)]
+   $Arguments=$null
+
+)
+
+$ScriptDir=[System.IO.Path]::GetDirectoryName($myInvocation.MyCommand.Definition)
+
+#$FolderWithScriptAndExe="C:\_SMSTSBlabla\Packages\CM10033"
+$xmlString=Get-Content ($ScriptDir+"\Start-Interactive.xml") | out-string
+$xmlString=$xmlString.Replace("xxDummyxx",$env:windir)
+$xmlString=$xmlString.Replace("start-dummy.ps1","start-"+$Name+".ps1")
+
+write-host $xmlString
+$PS1=Get-Content ($ScriptDir+"\Start-Dummy.ps1") -Raw
+#$PS1=$PS1.Replace("xxDummyxx",$ScriptDir)
+$PS1=$PS1.Replace("xxExexx",$FullPathToExe)
+$PS1=$PS1.Replace("xxLogNamexx",$Name)
+if ($Arguments)
+{
+write-host "Arguments not implemented yet."
+
+}
+
+$PS1 | Set-Content ($env:windir+"\start-"+$Name+".ps1")
+
+
+write-host $PS1
+
+Register-ScheduledTask -TaskName $Name -Xml ($xmlString)

eef6f40f3ce9c61c558931109812ccfc597edd6c
MattiasC85 <44426149+MattiasC85@users.noreply.github.com>
2019-06-03 17:08:09 +0200

    Update Import-Task.ps1

--- Start-Interactive/Import-Task.ps1
+++ Start-Interactive/Import-Task.ps1
@@ -12,7 +12,6 @@
 
 $ScriptDir=[System.IO.Path]::GetDirectoryName($myInvocation.MyCommand.Definition)
 
-#$FolderWithScriptAndExe="C:\_SMSTSBlabla\Packages\CM10033"
 $xmlString=Get-Content ($ScriptDir+"\Start-Interactive.xml") | out-string
 $xmlString=$xmlString.Replace("xxDummyxx",$env:windir)
 $xmlString=$xmlString.Replace("start-dummy.ps1","start-"+$Name+".ps1")
@@ -23,10 +22,6 @@ $PS1=Get-Content ($ScriptDir+"\Start-Dummy.ps1") -Raw
 $PS1=$PS1.Replace("xxExexx",$FullPathToExe)
 $PS1=$PS1.Replace("xxLogNamexx",$Name)
 $PS1=$PS1.Replace("xxArgumentsxx",$Arguments.ToString())
-if ($Arguments)
-{
-write-host ($Arguments.ToString())
-}
 
 $PS1 | Set-Content ($env:windir+"\start-"+$Name+".ps1")
 #write-host $PS1
